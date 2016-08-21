a8a3fa56c56e295f49327efc30c24192bee00333
Alexis Leon <alex@Alexiss-MacBook-Pro.local>
2016-08-20 19:36:29 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='read_env',
+	url='https://github.com/AlexisLeon/read_env',
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Alexis Leon',
+    author_email='alexis.leon@icloud.com',
+    packages=[],
+)
