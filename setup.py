c08ac229f648a44cfc8b9d3f24f06cfa00d79325
Alexis Leon <alex@Alexiss-MacBook-Pro.local>
2016-08-20 19:39:04 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-	name='read_env',
-	url='https://github.com/AlexisLeon/read_env',
-    version='1.0',
-    description='Python Distribution Utilities',
+    name='read_env',
+    version=__version__,
+    py_modules=['read_env'],
+    description='Read .env files for your python project',
+    long_description=read('README.md'),
     author='Alexis Leon',
     author_email='alexis.leon@icloud.com',
-    packages=[],
+    url='https://github.com/AlexisLeon/read_env',
+    install_requires=[],
+    license='MIT',
+    zip_safe=False,
+    keywords='env environment variables',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
 )
